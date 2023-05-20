1a0886e6743f41d91c38af158868dee5e56f31b9
hmateu <hmateu.ortola@gmail.com>
2023-05-20 16:07:04 +0200

    feat: Knows the position of the right balls

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 //Combinación ganadora
-let winnerChoice = [2, 4, 1, 5];
+let winnerChoice = [5, 3, 1, 5];
 //Combinación de cada jugada
-let currentChoice = [4, 2, 5, 1];
+let currentChoice = [2, 6, 1, 9];
 //Aciertos en cada jugada
 let matchBall = [];
+//Bola acertada en ubicación incorrecta
+let wrongPositionBall = [];
 //Cada acierto de bola en su ubicación correcta suma 1. Si llega a 4 es porque ha ganado en modo fácil
 let successBall = 0;
+//Cada acierto de bola que no este en su ubicación correcta suma 1.
+let unSuccessBall = 0;
 //Guarda la posición de la bola que se ha acertado
 let whenMatch = [];
+//Guarda la posición de la bola que se ha acertado el color pero no la posición
+let whenUnMatch = [];
 
 //Cada vez que haya una coincidencia sumo 1 a successBall
-winnerChoice.map(
-    (element, i) => {
-        //Si la bola de winnerChoice es igual a la de currentChoice
-        if (element == currentChoice[i]) {
-            matchBall[i] = "true";
-            successBall++;
-        }else{
-            //Si la bola es diferente, la comparo en el resto de bolas que quedan del currentChoice
-            for(let j=i++;j<=currentChoice.length;j++){
-                if(element == currentChoice[j]){
-
-                }
-            }
-        }
-    })
+winnerChoice.map((element, i) => {
+    //Si la bola de winnerChoice es igual a la de currentChoice
+    if (element == currentChoice[i]) {
+        console.log("MATCH")
+        matchBall[i] = "true";
+        successBall++;
+    } else {
+        //Si la bola es diferente, la comparo en el resto de bolas que quedan del currentChoice
+        console.log("NO MATCH")
+        // for (let j = i++; j <= currentChoice.length; j++) {
+        //     if (element == currentChoice[j]) {
+        //         wrongPositionBall[i] = "true";
+        //         unSuccessBall++;
+        //     }
+        // }
+    }
+})
 
 //Averiguar si ha ganado la partida o continua jugando
 if (successBall == 4) {
-    alert("Ha ganado la partida");
+    console.log("Ha ganado la partida");
 } else {
-    alert("Sigue probando");
+    console.log("Sigue probando");
 
     //Sacar la posición de las bolas que se han acerado.
     matchBall.map((element, i) => {
@@ -38,7 +46,18 @@ if (successBall == 4) {
             whenMatch.push(i);
         }
     })
-    alert(`Hay ${whenMatch.length} bolas en la posición correcta`);
+    //Sacar la posición de las bolas que se ha acertado el color pero no la posición
+
+    for(i=0;i<whenMatch.length;i++){
+        if(i==0){
+            console.log(`La posición donde se encuentran las bolas que has acertado son:`);
+            console.log(whenMatch[i]);
+        }else{
+            console.log(whenMatch[i]);
+        }
+
+    }
+    console.log(`Hay ${whenMatch.length} bolas en la posición correcta`);
 }
 
 //Averigua cuantas bolas hay en la winnerChoice, aunque no esten en el lugar correcto
