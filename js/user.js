20bd67fa6701e3820fa0047f6a39bb1f905efafa
hmateu <hmateu.ortola@gmail.com>
2023-05-22 08:58:14 +0200

    feat: Adds an error message if you do not enter any names

--- js/user.js
+++ js/user.js
@@ -1,6 +1,11 @@
 const sendUser = () => {
     let user = document.getElementById("name").value;
-    window.sessionStorage.setItem("user",user);
-    document.getElementById("userForm").classList.add("hidden");
-    document.getElementById("showLevels").classList.remove("hidden");
+    if(user != "" && user != null){
+        window.sessionStorage.setItem("user",user);
+        document.getElementById("userForm").classList.add("hidden");
+        document.getElementById("showLevels").classList.remove("hidden");
+        document.getElementById("errorMessage").classList.add("hidden");
+    }else{
+        document.getElementById("errorMessage").classList.remove("hidden");
+    }
 }
