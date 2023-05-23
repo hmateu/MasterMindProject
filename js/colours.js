d3b35aa80b7ed20a43c576989772b5763cf9a0a9
hmateu <hmateu.ortola@gmail.com>
2023-05-23 17:39:33 +0200

    feat: Color Picker changes the background div

--- js/colours.js
+++ js/colours.js
@@ -1,8 +1,20 @@
 let difficulty = window.sessionStorage.getItem("difficulty");
 if(difficulty == "easyLvl"){
     document.getElementById("easy").classList.remove("hidden");
+    window.sessionStorage.setItem("difficulty",difficulty);
 } else if(difficulty == "mediumLvl"){
     document.getElementById("medium").classList.remove("hidden");
+    window.sessionStorage.setItem("difficulty",difficulty);
 } else{
     document.getElementById("hard").classList.remove("hidden");
+    window.sessionStorage.setItem("difficulty",difficulty);
 }
+
+const colorPickers = document.querySelectorAll("#easy .inputColours");
+const colorBall = document.querySelectorAll("#easy .colour");
+colorPickers.forEach((picker, i) => {
+    picker.addEventListener("input", (e) => {
+        const color = e.target.value;
+        colorBall[i].style.background = color;
+    })
+})
