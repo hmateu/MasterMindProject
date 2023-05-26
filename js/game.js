307e461024dbc430d576582a4979799805e87fa4
hmateu <hmateu.ortola@gmail.com>
2023-05-26 20:57:22 +0200

    feat: Assigns id to each row and cell of the board

--- js/game.js
+++ js/game.js
@@ -15,10 +15,15 @@ switch (nRows) {
 
 //Crea las filas del tablero
 const createRow = () => {
+    let idRow = 0;
+    let idBall = 0;
+    let idHintBall = 0;
     let board = document.getElementById("board");
 
     let row = document.createElement("div");
     row.className = "row";
+    row.id = `row${idRow}`;
+    idRow++;
 
     let balls = document.createElement("div");
     balls.className = "balls";
@@ -26,7 +31,9 @@ const createRow = () => {
     for (let i = 0; i < 4; i++) {
         let ball = document.createElement("div");
         ball.className = "ball";
-        balls.appendChild(ball)
+        ball.id = `row${idRow}-ball${idBall}`;
+        balls.appendChild(ball);
+        idBall++;
     }
 
     let hintBalls = document.createElement("div");
@@ -35,12 +42,17 @@ const createRow = () => {
     for (let i = 0; i < 4; i++) {
         let hintBall = document.createElement("div");
         hintBall.className = "hintBall";
+        hintBall.id = `row${idRow}-hintBaill${idHintBall}`
         hintBalls.appendChild(hintBall);
+        console.log(hintBall.id)
+        idHintBall++;
     }
 
     row.appendChild(balls);
     row.appendChild(hintBalls);
     board.appendChild(row);
+
+    
 }
 
 //Pinta el tablero en pantalla dependiendo de la dificultad
@@ -50,4 +62,4 @@ const paintBoard = nRows => {
     }
 }
 
-paintBoard(nRows);
+paintBoard(nRows); 
